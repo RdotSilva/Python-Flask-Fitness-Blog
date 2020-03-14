820e1fbf521e3dff2de03b4c65bbc8ea880f8892
RdotSilva <RyandotSilva@gmail.com>
2020-03-14 11:37:39 -0400

    Add flaskblog.py

---
+++ flaskblog.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Ryan Silva",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2020",
+    },
+    {
+        "author": "Mike Silva",
+        "title": "Blog Post 2",
+        "content": "2nd post content",
+        "date_posted": "May 2, 2020",
+    },
+]
+
+# Handle multiple routes using the same function
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+# Run in debug mode, avoiding the use of ENV variable with the flask run command
+if __name__ == "__main__":
+    app.run(debug=True)
