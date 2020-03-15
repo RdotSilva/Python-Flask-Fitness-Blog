d23f3170525504bdf5a92bdff665c26c5529d720
RdotSilva <RyandotSilva@gmail.com>
2020-03-15 16:21:17 -0400

    Add DB instance

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
 
 # Temporary secret key, will change this and make env variable later
-app.config["SECRET_KEY"] = "b745125d03ae68e8a939473072208d98"
+app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+
+# Create DB instance
+db = SQLAlchemy(app)
 
 posts = [
     {
