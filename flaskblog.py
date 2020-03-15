df01f1e2853e9c86f3c8fdada3fff9044295bbae
RdotSilva <RyandotSilva@gmail.com>
2020-03-15 16:29:07 -0400

    Add User model

--- flaskblog.py
+++ flaskblog.py
@@ -12,6 +12,18 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 # Create DB instance
 db = SQLAlchemy(app)
 
+# DB models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
 posts = [
     {
         "author": "Ryan Silva",
