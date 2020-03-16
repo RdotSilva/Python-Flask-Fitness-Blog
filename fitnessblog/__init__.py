556ef1fffc0fb5bfd4da4ecfe0ae6162f729e974
RdotSilva <RyandotSilva@gmail.com>
2020-03-16 11:41:39 -0400

    Add Bcrypt init

--- fitnessblog/__init__.py
+++ fitnessblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -10,5 +11,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 
 # Create DB instance
 db = SQLAlchemy(app)
+
+# Bcrypt PW hashing
+bcrypt = Bcrypt(app)
 
 from fitnessblog import routes
