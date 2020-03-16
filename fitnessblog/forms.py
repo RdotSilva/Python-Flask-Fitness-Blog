cf07afd842a23a70cff2bea569c18f69b6981b3a
RdotSilva <RyandotSilva@gmail.com>
2020-03-16 12:08:00 -0400

    Add validate_username method

--- fitnessblog/forms.py
+++ fitnessblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fitnessblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -13,6 +14,13 @@ class RegistrationForm(FlaskForm):
         "ConfirmPassword", validators=[DataRequired(), EqualTo("password")]
     )
     submit = SubmitField("Sign Up")
+
+    # Check if username exists in db
+    def validate_username(self, username):
+        # Check for user and get first one there, if not return none
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username is taken. Choose another username.")
 
 
 class LoginForm(FlaskForm):
