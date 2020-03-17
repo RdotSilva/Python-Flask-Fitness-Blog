3a4b7ea63a1faabd1ef46e7bae3754d2d23c375c
RdotSilva <RyandotSilva@gmail.com>
2020-03-17 12:19:00 -0400

    Add account form to account route

--- fitnessblog/routes.py
+++ fitnessblog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request
-from fitnessblog.forms import RegistrationForm, LoginForm
+from fitnessblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from fitnessblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -95,6 +95,9 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+    form = UpdateAccountForm()
     # Set profile picture image file
     image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
-    return render_template("account.html", title="Account", image_file=image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
