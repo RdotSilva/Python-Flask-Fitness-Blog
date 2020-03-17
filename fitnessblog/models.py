f7aab99b4fb41688c1e51fc0ed7012e372cbfc66
RdotSilva <RyandotSilva@gmail.com>
2020-03-16 21:13:49 -0400

    Add UserMixin to User model

--- fitnessblog/models.py
+++ fitnessblog/models.py
@@ -1,5 +1,6 @@
 from fitnessblog import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
 
 @login_manager.user_loader
@@ -8,7 +9,7 @@ def load_user(user_id):
 
 
 # DB models
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
