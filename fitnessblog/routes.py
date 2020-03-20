a202435190b6b791cb8b8e3f76a067a867bfac57
RdotSilva <RyandotSilva@gmail.com>
2020-03-20 13:28:34 -0400

    Update reset_request to send user email

--- fitnessblog/routes.py
+++ fitnessblog/routes.py
@@ -220,6 +220,11 @@ def user_posts(username):
     return render_template("user_posts.html", posts=posts, user=user)
 
 
+def send_reset_email(user):
+    # TODO
+    pass
+
+
 # Create a request for password reset
 @app.route("/reset_password", methods=["GET", "POST"])
 def reset_request():
@@ -227,9 +232,12 @@ def reset_request():
     if current_user.is_authenticated:
         return redirect(url_for("home"))
     form = RequestResetForm()
-    # If valid form is submitted get the user based off of email
+    # If valid form is submitted get the user based off of email, and send that user an email with token
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash("An email has been sent with instructions to reset your password", "info")
+        return redirect(url_for("login"))
     return render_template("reset_request.html", title="Reset Password", form=form)
 
 
