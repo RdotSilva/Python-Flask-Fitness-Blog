10eb7fc73949d5df2dc0436b3c7cc174024797d0
RdotSilva <RyandotSilva@gmail.com>
2020-03-20 13:07:45 -0400

    Add verify_reset_token method

--- fitnessblog/models.py
+++ fitnessblog/models.py
@@ -25,6 +25,16 @@ class User(db.Model, UserMixin):
         s = Serializer(app.config[SECRET_KEY], expires_sec)
         return s.dumps({"user_id": self.id}).decode("utf-8")
 
+    # Verify the reset token and return user if good
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config[SECRET_KEY])
+        try:
+            user_id = s.load(token)["user_id"]
+        except:
+            return None
+        return User.query.get(user_id)
+
     # Used to indicate how the user will look when printed
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
