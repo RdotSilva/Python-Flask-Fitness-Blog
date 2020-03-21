4a844d05993814870ba744941afe07241ae75e5e
RdotSilva <RyandotSilva@gmail.com>
2020-03-21 17:52:28 -0400

    Move posts routes

---
+++ fitnessblog/posts/routes.py
@@ -0,0 +1,68 @@
+from flask import Blueprint
+
+posts = Blueprint("posts", __name__)
+
+# Create a new post
+@app.route("/post/new", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        # Construct new post and save to db
+        post = Post(
+            title=form.title.data, content=form.content.data, author=current_user
+        )
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post has been created!", "success")
+        return redirect(url_for("home"))
+    return render_template(
+        "create_post.html", title="New Post", form=form, legend="New Post"
+    )
+
+
+# Get a specific post by id
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+# Update a post
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    # Check if user owns post before updating
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    # Validate form data, update db, redirect to post page
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for("post", post_id=post.id))
+    # If get request, populate form with the current values from db
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template(
+        "create_post.html", title="Update Post", form=form, legend="Update Post"
+    )
+
+
+# Delete a post
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    # Check if user owns post before updating
+    if post.author != current_user:
+        abort(403)
+    # Remove post from db, flash message, redirect to home
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", "success")
+    return redirect(url_for("home"))
