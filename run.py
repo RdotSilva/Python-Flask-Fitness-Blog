4772f9f1c2ffbfe37c14f6ac9a48e03068b91e6e
RdotSilva <RyandotSilva@gmail.com>
2020-03-22 17:46:33 -0400

    Create new instance of app in run

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from fitnessblog import app
+from fitnessblog import create_app
+
+# Create a new app instance
+app = create_app()
 
 # Run in debug mode, avoiding the use of ENV variable with the flask run command
 if __name__ == "__main__":
