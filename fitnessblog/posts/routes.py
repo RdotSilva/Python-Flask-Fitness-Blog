20979414871de2fd52ca8697aa1f1db08bb55e24
RdotSilva <RyandotSilva@gmail.com>
2020-03-23 11:55:14 -0400

    Update post routes to handle category

--- fitnessblog/posts/routes.py
+++ fitnessblog/posts/routes.py
@@ -14,7 +14,10 @@ def new_post():
     if form.validate_on_submit():
         # Construct new post and save to db
         post = Post(
-            title=form.title.data, content=form.content.data, author=current_user
+            title=form.title.data,
+            content=form.content.data,
+            category=form.category.data,
+            author=current_user,
         )
         db.session.add(post)
         db.session.commit()
@@ -45,6 +48,7 @@ def update_post(post_id):
     if form.validate_on_submit():
         post.title = form.title.data
         post.content = form.content.data
+        post.category = form.category.data
         db.session.commit()
         flash("Your post has been updated!", "success")
         return redirect(url_for("posts.post", post_id=post.id))
@@ -52,6 +56,7 @@ def update_post(post_id):
     elif request.method == "GET":
         form.title.data = post.title
         form.content.data = post.content
+        post.category = form.category.data
     return render_template(
         "create_post.html", title="Update Post", form=form, legend="Update Post"
     )
