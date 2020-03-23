80d4940b55a3ed91aa7dd92c97728c7b7d688951
RdotSilva <RyandotSilva@gmail.com>
2020-03-23 12:39:51 -0400

    Update run to handle create new db

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
-from fitnessblog import create_app
+from fitnessblog import create_app, db
 
 # Create a new app instance
 app = create_app()
+
+# Add this in to recreate the database. Comment out once database is created
+app_ctx = app.app_context()
+app_ctx.push()
+db.create_all()
+app_ctx.pop()
 
 # Run in debug mode, avoiding the use of ENV variable with the flask run command
 if __name__ == "__main__":
