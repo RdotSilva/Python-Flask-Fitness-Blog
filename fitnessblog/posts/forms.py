d1d7005d827ab765e066445a205f3b9a9d4d6b4a
RdotSilva <RyandotSilva@gmail.com>
2020-03-23 11:53:48 -0400

    Add category select field

--- fitnessblog/posts/forms.py
+++ fitnessblog/posts/forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     content = TextAreaField("Content", validators=[DataRequired()])
+    category = SelectField(
+        "Post Category",
+        choices=[
+            ("cardio", "Cardio"),
+            ("weight", "Weight Training"),
+            ("diet", "Diet"),
+            ("other", "Other"),
+        ],
+    )
     submit = SubmitField("Post")
